e43e4bc77aef1695d8a524837d278f7758a1b92a
caiquectt <caiquectt@gmail.com>
2023-05-03 09:03:17 -0300

    Adição de um time falso caso times sejam impares

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 var teams = []
+var rounds;
+var half;
 
 function getTeams(){
   teams = []
   let teamsToSplit = document.querySelector("#textareaTeams").value;
   teamsInfo = teamsToSplit.split("\n");
+    if (teamsInfo.length % 2 == 1){
+      teamsInfo.push("BYE;Descanso");
+    }
+  rounds = teamsInfo.length - 1;
+  half = teamsInfo.length / 2;
   teamsInfo.map((info) => {
-    if (info !== ''){
-    teams.push({
-      name: info.split(";")[0],
-      state: info.split(";")[1]
-    })
-  }})
+    if (info == ''){
+      teams.push({
+        name: 'BYE',
+        state: 'Descanso'
+      })
+    }
+    else{
+      teams.push({
+        name: info.split(";")[0],
+        state: info.split(";")[1]
+      })
+    }
+  })
   console.log(teams)
 }
 
